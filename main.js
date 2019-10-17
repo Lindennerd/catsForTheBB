0b36391f727b1cf9e518aeb8ca8c60631b6817bd
luiz.paulo <luiz.paulo@powerbrasil.com.br>
2019-10-17 17:30:16 -0300

    gactos

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ async function anotherGacto() {
 }
 
 async function getGacto() {
+	let spinner = document.querySelector('#cover-spin');
+	spinner.style.display = 'block';
+
 	let urlDosGactos = 'https://api.thecatapi.com/v1/images/search';
 	let response = await fetch(urlDosGactos);
     let cat = await response.json();
@@ -19,4 +22,7 @@ async function getGacto() {
 function showGacto(gacto) {
 	let gactoEl = document.querySelector('#gacto');
 	gactoEl.src = gacto.url;
+	
+	let spinner = document.querySelector('#cover-spin');
+	spinner.style.display = 'none';
 }
